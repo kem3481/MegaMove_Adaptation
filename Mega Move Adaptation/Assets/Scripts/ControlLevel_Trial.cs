2507be870900ac23414d2f033fba7684ebd1f7cb
Kelsey <kem3481@rit.edu>
2019-05-31 08:40:54 -0400

    Initial COmmit

---
+++ Mega Move Adaptation/Assets/Scripts/ControlLevel_Trial.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using USE_States;
+
+public class ControlLevel_Trial : ControlLevel
+{
+    public GameObject beginText;
+    public GameObject smallOverlap;
+    public GameObject mediumOverlap;
+    public GameObject largeOverlap;
+
+    private int smallRadius = 10;
+    private int mediumRadius = 20;
+    private int largeRadius = 30;
+    private int radius;
+
+    private GameObject[] targets;
+    private GameObject[] radii;
+    private GameObject testobject;
+
+    private VerifyPositions verifyPositions;
+
+    public override void DefineControlLevel()
+    {
+        // Defining States
+        State positionReady = new State("Positions are correct");
+        State stimOn = new State("Stimulus");
+        AddActiveStates(new List<State> { positionReady, stimOn });
+
+        // Accessing other scripts
+        ControlLevel_Trial end = transform.GetComponent<ControlLevel_Trial>();
+        positionReady.AddChildLevel(end);
+
+        verifyPositions = GetComponent<VerifyPositions>();
+
+        // Making a list of target overlap options
+        targets = new GameObject[3];
+        targets[0] = smallOverlap;
+        targets[1] = mediumOverlap;
+        targets[2] = largeOverlap;
+
+        // List of radii
+        radii = new GameObject[3];
+        radii[0] = smallRadius;
+        radii[1] = mediumRadius;
+        radii[2] = largeRadius;
+        radius = radii[UnityEngine.Random.Range(0, 2)];
+
+        positionReady.AddInitializationMethod(() =>
+        {
+            beginText.SetActive(true);
+            Debug.Log("Setting Positions");
+
+            if (verifyPositions.positionCorrect !=null)
+            {
+                beginText.SetActive(false);
+                Debug.Log("Positions are correct");
+            }
+        });
+        positionReady.SpecifyTermination(() => end.Terminated == true, stimOn);
+
+        stimOn.AddInitializationMethod(() =>
+        {
+            testobject = Instantiate(targets[UnityEngine.Random.Range(0, 2)]);
+            testobject.transform.position = new Vector3( radius*Mathf.Cos(testobject.transform.rotation.z), radius*Mathf.Sin(testobject.transform.rotation.z), 0f);
+            testobject.transform.rotation = new Vector3(0f, 0f, UnityEngine.Random.Range(0, 359));
+        });
+    }
+}
---
+++ Mega Move Adaptation/Assets/Scripts/VerifyPositions.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using USE_States;
+
+public class VerifyPositions : ControlLevel
+{
+    // Position is correct game object
+    public GameObject positionCorrect;
+
+    // Physical Objects
+    private GameObject rightController;
+    private GameObject head;
+
+    // Starting Positions
+    private GameObject handSphere;
+    private GameObject headColumn;
+
+    //  Verification Lights
+    private GameObject unlitHead;
+    private GameObject litHead;
+    private GameObject unlitHand;
+    private GameObject litHand;
+
+    // Light prefabs
+    public GameObject LightPrefab_unlit;
+    public GameObject LightPrefab_lit;
+
+
+    public override void DefineControlLevel()
+    {
+        State verify = new State("Verified");
+        State wait = new State("Waiting");
+        State end = new State("Proceeding");
+        AddActiveStates(new List<State> { verify, wait, end });
+
+        verify.AddInitializationMethod(() =>
+       {
+           // Initially the position is not correct
+           positionCorrect.SetActive(false);
+
+           // Adding physical objects to game objects
+           rightController = GameObject.FindGameObjectWithTag("RightController");
+           head = GameObject.FindGameObjectWithTag("Camera");
+
+           // Creating unlit position verification lights
+           unlitHand = Instantiate(LightPrefab_unlit);
+           unlitHand.transform.position = new Vector3(0f, 0f, .5f);
+
+           unlitHead = Instantiate(LightPrefab_unlit);
+           unlitHead.transform.position = new Vector3(0f, 0f, -.5f);
+
+           // If the controller is within the sphere that designates beginning hand position and the unlit hand prefab exists, destroy the unlit hand prefab and create lit hand prefab
+           if (unlitHand != null &&
+               rightController.transform.localPosition.x > .20 &&
+               rightController.transform.localPosition.x < .30 &&
+               rightController.transform.localPosition.y > 1.1 &&
+               rightController.transform.localPosition.y < 1.2 &&
+               rightController.transform.localPosition.z > -.3 &&
+               rightController.transform.localPosition.z < -.2)
+           {
+               Destroy(unlitHand);
+               litHand = Instantiate(LightPrefab_lit);
+               litHand.transform.position = new Vector3(0f, 0f, .5f);
+           }
+
+           // If the controller is not within the start sphere and the lithand prefab exists, destory the lit hand prefab and create the unlit hand prefab
+           if (litHand != null &&
+              (rightController.transform.localPosition.x < .20 ||
+              rightController.transform.localPosition.x > .30 ||
+              rightController.transform.localPosition.y < 1.1 ||
+              rightController.transform.localPosition.y > 1.2 ||
+              rightController.transform.localPosition.z < -.3 ||
+              rightController.transform.localPosition.z > -.2))
+           {
+               Destroy(litHand);
+               unlitHand = Instantiate(LightPrefab_unlit);
+               unlitHand.transform.position = new Vector3(0f, 0f, .5f);
+           }
+
+           // If the headset is within the start column and the lithead prefab does not exist, destory the unlit head prefab and create the lit head prefab
+           if (unlitHead != null &&
+              head.transform.localPosition.x > -.05 &&
+              head.transform.localPosition.x < .05 &&
+              head.transform.localPosition.y > 1.2 &&
+              head.transform.localPosition.y < 1.6 &&
+              head.transform.localPosition.z > -.55 &&
+              head.transform.localPosition.z < -.45)
+           {
+               Destroy(unlitHead);
+               litHead = Instantiate(LightPrefab_lit);
+               litHead.transform.position = new Vector3(0f, 0f, -.5f);
+           }
+
+           // If the headset is not within the start column and the lithead prefab does exist, destory the lit head prefab and create the unlit head prefab
+           if (litHead != null &&
+              (head.transform.localPosition.x < -.05 ||
+              head.transform.localPosition.x > .05 ||
+              head.transform.localPosition.y < 1.2 ||
+              head.transform.localPosition.y > 1.6 ||
+              head.transform.localPosition.z < -.55 ||
+              head.transform.localPosition.z > -.45))
+           {
+               Destroy(litHead);
+               unlitHead = Instantiate(LightPrefab_unlit);
+               unlitHead.transform.position = new Vector3(0f, 0f, -.5f);
+           }
+
+           // If both are in correct position, set the position correct gameobject to true
+           if (litHead != null && litHand != null)
+           {
+               positionCorrect.SetActive(true);
+           }
+           
+       });
+        verify.SpecifyTermination(() => positionCorrect != null, wait);
+        
+        wait.AddTimer(3f, end);
+
+        end.AddInitializationMethod(() =>
+        {
+            this.AddTerminationSpecification(() => positionCorrect != null);
+        });
+    }
+}

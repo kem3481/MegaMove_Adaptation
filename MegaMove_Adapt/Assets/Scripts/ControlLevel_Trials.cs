639788249c64e22a9417d02820836a57c953eef9
kem3481 <kem3481@rit.edu>
2019-06-05 14:25:31 -0400

    point system working

--- MegaMove_Adapt/Assets/Scripts/ControlLevel_Trials.cs
+++ MegaMove_Adapt/Assets/Scripts/ControlLevel_Trials.cs
@@ -15,14 +15,18 @@ public class ControlLevel_Trials : ControlLevel
     public GameObject manager; // control levels game object, holding all scirpts
     public GameObject beginText; // instructions canvas
     public GameObject endText; // thank you for playing canvas
+    public Text scoreDisplay;
+    private GameObject penalty;
 
     private Verify verifyPositions; // script
     private Controls controls; // script
     private ControllerCheck controller; // scripts
     private HeadCheck head; // scripts
     private TriggerPull triggered; // scripts
+    private PointSystem points; // scripts
     private int trials = 0; // number of total trials
-    private int score = 0; // player score
+    private int score; // player score
+    private int trialScore;
     private int end = 0;
 
     [System.NonSerialized]
@@ -30,7 +34,7 @@ public class ControlLevel_Trials : ControlLevel
     [System.NonSerialized]
     public float angle;
     [System.NonSerialized]
-    public float radius;
+    public float radius, trigger_x, trigger_y, trigger_z, target_x, target_y, target_z, penalty_x, penalty_y, penalty_z;
 
     // Vive Control GameObjects
     public SteamVR_Input_Sources handType;
@@ -42,9 +46,10 @@ public class ControlLevel_Trials : ControlLevel
         State begin = new State("Begin"); // Step 1 and 2 in Procedure
         State stimOn = new State("Stimulus"); // Step 3
         State collectResponse = new State("CollectResponse"); // Step 4
+        State scoreState = new State("Score");
         State destination = new State("Destination"); // sends the script either back to begin or to feeback
         State feedback = new State("Feedback"); // Step 5
-        AddActiveStates(new List<State> { begin, stimOn, collectResponse, destination, feedback });
+        AddActiveStates(new List<State> { begin, stimOn, collectResponse, scoreState, destination, feedback });
         
         // Accessing other scripts
         verifyPositions = manager.GetComponent<Verify>();
@@ -52,12 +57,31 @@ public class ControlLevel_Trials : ControlLevel
         controller = controllerPosition.GetComponent<ControllerCheck>();
         head = playerPosition.GetComponent<HeadCheck>();
         triggered = test.GetComponent<TriggerPull>();
+        points = manager.GetComponent<PointSystem>();
+
+        scoreDisplay.text = "Score: " + score;
 
         begin.AddStateInitializationMethod(() =>
         {
+            if (trials == 0)
+            {
+                score = 0;
+            }
+
+            trialScore = 0;
             controllerPosition.SetActive(true);
             beginText.SetActive(true);
             endText.SetActive(false);
+
+            trigger_x = 0;
+            trigger_y = 0;
+            trigger_z = 0;
+            target_x = 0;
+            target_y = 0;
+            target_z = 0;
+            penalty_x = 0;
+            penalty_y = 0;
+            penalty_z = 0;
             
         });
         begin.SpecifyStateTermination(() => verifyPositions.positionsCorrect, stimOn);
@@ -78,6 +102,8 @@ public class ControlLevel_Trials : ControlLevel
                 testobject.transform.eulerAngles = new Vector3(0f, 0f, angle);
             }
 
+            penalty = GameObject.FindGameObjectWithTag("PenaltyonTarget");
+
             Debug.Log("Overlap: " + testobject);
             Debug.Log("Radius: " + radius);
             Debug.Log("Angle: " + angle);
@@ -87,19 +113,57 @@ public class ControlLevel_Trials : ControlLevel
         
         collectResponse.AddUpdateMethod(() =>
         {
-            
+
             if (triggered.trigger == true)
             {
-                Destroy(testobject);
-                Debug.Log("Trigger pull position: " + test.transform.localPosition.x + ", " + test.transform.localPosition.y);
-                Debug.Log("Target position: " + testobject.transform.localPosition.x + ", " + testobject.transform.localPosition.y);
-            }
-        });
-        collectResponse.SpecifyStateTermination(() => testobject == null, destination);
+                trigger_x = gamecontroller.transform.position.x;
+                trigger_y = gamecontroller.transform.position.y;
+                trigger_z = gamecontroller.transform.position.z;
+                target_x = testobject.transform.localPosition.x;
+                target_y = testobject.transform.localPosition.y;
+                target_z = testobject.transform.localPosition.z;
+                penalty_x = penalty.transform.position.x;
+                penalty_y = penalty.transform.position.y;
+                penalty_z = penalty.transform.position.z;
 
+                Destroy(testobject);
+            }
+            
+        });
+        collectResponse.SpecifyStateTermination(() => testobject == null, scoreState);
+
+        scoreState.AddStateInitializationMethod(() =>
+        {
+            if ((trigger_x > (target_x - .1f)) && (trigger_x < (target_x + .1f)) &&
+               (trigger_y > (target_y - .1f)) && (trigger_y < (target_y + .1f)) &&
+               (trigger_z > (target_z - .1f)) && (trigger_z < (target_z + .1f)))
+            {
+                trialScore = 100;
+            }
+            else
+            {
+                trialScore = 0;
+            }
+
+            if ((trigger_x > (penalty_x - .1f)) && (trigger_x < (penalty_x + .1f)) &&
+               (trigger_y > (penalty_y - .1f)) && (trigger_y < (penalty_y + .1f)) &&
+               (trigger_z > (penalty_z - .1f)) && (trigger_z < (penalty_z + .1f)))
+            {
+                trialScore = -100;
+            }
+            
+        });
+        scoreState.AddTimer(1f, destination);
 
         destination.AddStateInitializationMethod(() =>
         {
+            score = score + trialScore;
+            scoreDisplay.text = "Score: " + score;
+
+            Debug.Log("Trigger pull position: " + trigger_x + ", " + trigger_y + ", " + trigger_z);
+            Debug.Log("Target position: " + target_x + ", " + target_y + ", " + target_z);
+            Debug.Log("Score: " + score);
+
             if (trials < 90)
             {
                 end = 1;
